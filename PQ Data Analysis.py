ef3f90e9078149c50975af2a3ea46d71e397254a
Gautam023 <mvg_gautam@yahoo.com>
2016-05-05 13:55:30 +0530

    First Commit

--- PQ Data Analysis.py
+++ PQ Data Analysis.py
@@ -10,16 +10,16 @@ import numpy as np
 import math
 
 #Desigin Filter
-def build_filter(Q,n,Vs,QF):
+def build_filter(n,Vs,QF):
     w = 2*3.141*50
-    C = Q/(n*w*(Vs^2))
-    L = 1/(n*n*w*w*C)
+    Q = (8e-6)*(2*w*(Vs^2))
+    L = 1/(n*n*w*w*8e-6)
     R = (n*w*L)/QF
-    return C,L,R
+    return Q,L,R
 
 #Checking for Harmonic Resonance
 def check_harmonic(C,L,n):
-    if 50 == 1/(2*3.141*n*math.sqrt(C*L)):
+    if 50 == int(1/(2*3.141*n*math.sqrt(8e-6*L))):
         return True
     else:
         return False
@@ -57,28 +57,40 @@ IHD3_data2 = first_sheet_2.col_values(colx=3,start_rowx=1,end_rowx=157)
 IHD3_data3 = first_sheet_3.col_values(colx=3,start_rowx=1,end_rowx=157)
 IHD3_data4 = first_sheet_4.col_values(colx=3,start_rowx=1,end_rowx=157)
 
+IHD3_data1_avg = np.mean(IHD3_data1)
+IHD3_data2_avg = np.mean(IHD3_data2)
+IHD3_data3_avg = np.mean(IHD3_data3)
+IHD3_data4_avg = np.mean(IHD3_data4)
+
 IHD5_data1 = first_sheet_1.col_values(colx=4,start_rowx=1,end_rowx=157)
 IHD5_data2 = first_sheet_2.col_values(colx=4,start_rowx=1,end_rowx=157)
 IHD5_data3 = first_sheet_3.col_values(colx=4,start_rowx=1,end_rowx=157)
 IHD5_data4 = first_sheet_4.col_values(colx=4,start_rowx=1,end_rowx=157)
+
+IHD5_data1_avg = np.mean(IHD5_data1)
+IHD5_data2_avg = np.mean(IHD5_data2)
+IHD5_data3_avg = np.mean(IHD5_data3)
+IHD5_data4_avg = np.mean(IHD5_data4)
+
+
 
 #Plotting
 data_plot(IHD3_data1,IHD3_data2,IHD3_data3,IHD3_data4,'IHD3',1)
 data_plot(IHD5_data1,IHD5_data2,IHD5_data3,IHD5_data4,'IHD5',2)
 
 #Filter Design
-reactive_power = first_sheet_4.col_values(colx=9,start_rowx=1,end_rowx=157)
+reactive_power = first_sheet_1.col_values(colx=9,start_rowx=1,end_rowx=157)
 Reactive_power = (reactive_power)
 avg_reactive_power = np.mean(Reactive_power)
 max_reactive_power = max(reactive_power)
-filter_values_3 = build_filter(max_reactive_power,3,221,50)
-filter_values_5 = build_filter(max_reactive_power,5,221,50)
+filter_values_3 = build_filter(3,221,50)
+filter_values_5 = build_filter(5,221,50)
 harmonic_resonance_3 = check_harmonic(filter_values_3[0],filter_values_3[1],3)
 harmonic_resonance_5 = check_harmonic(filter_values_5[0],filter_values_5[1],5)
 
 #Logging
 log = open("C:\Users\gautam\Desktop\PQ Datalog.txt","a")
-log.write("Max Reactive Power: "+str(max_reactive_power))
+log.write("Avg Reactive Power: "+str(avg_reactive_power))
 log.write("\nFilter values for 3rd harmonic component: "  +str(filter_values_3))
 log.write("\nFilter values for 5th harmonic component: "  +str(filter_values_5))
 log.write("\nHarmonic Resonance for 3rd harmonic filter: "+str(harmonic_resonance_3))
